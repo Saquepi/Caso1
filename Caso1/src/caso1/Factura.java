ece9eb754ec61ff493e4947d30e0910279f5d85d
Saquepi <145624844+Saquepi@users.noreply.github.com>
2023-10-25 20:48:18 -0600

    Add files via upload

---
+++ Caso1/src/caso1/Factura.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caso1;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class Factura {
+    private String client;
+    private String id;
+    private int facNum;
+    private double facAmount;
+    private int month;
+    private int year;
+    private int pending;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getFacNum() {
+        return facNum;
+    }
+
+    public void setFacNum(int facNum) {
+        this.facNum = facNum;
+    }
+
+    public double getFacAmount() {
+        return facAmount;
+    }
+
+    public void setFacAmount(double facAmount) {
+        this.facAmount = facAmount;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+    
+    public String MonthName(){
+        if (month==1){
+            return "Enero";
+        }else{
+            return "NULL";
+        }
+        
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPending() {
+        return pending;
+    }
+
+    public void setPending(int pending) {
+        this.pending = pending;
+    }
+    
+    
+}
